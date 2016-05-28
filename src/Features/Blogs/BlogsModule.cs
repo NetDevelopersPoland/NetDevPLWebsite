8bb616614d5096bcdcf301befe6938721068a5dd
Pawel Klimczyk <pawel@klimczyk.pl>
2016-05-28 09:57:21 +0200

    Added PoC for sample Feature (Blogs)

---
+++ src/Features/Blogs/BlogsModule.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Nancy;
+using Newtonsoft.Json;
+
+namespace NetDevPLWeb.Features.Blogs
+{
+    public class BlogsModule : NancyModule
+    {
+        BlogSource source = new BlogSource();
+
+        public BlogsModule()
+        {
+            Get["/blogs"] = parameters =>
+            {
+                var blogs = source.GetBlogs();
+                return View["blogList", new BlogListViewModel(blogs)];
+            };
+        }
+    }
+
+    public class BlogSource
+    {
+        public List<Blog> GetBlogs()
+        {
+            string json = File.ReadAllText("Features/Blogs/blogs.json");
+            var blogs = JsonConvert.DeserializeObject<List<Blog>>(json);
+
+            //Randomize order to not favorize any
+            return blogs.OrderBy(a => Guid.NewGuid()).ToList();
+        }
+    }
+
+    public class Blog
+    {
+        public string Url { get; set; }
+        public string Rss { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class BlogListViewModel
+    {
+        public BlogListViewModel(List<Blog> blogs)
+        {
+            Blogs = blogs;
+        }
+
+        public List<Blog> Blogs { get; set; }
+    }
+}
