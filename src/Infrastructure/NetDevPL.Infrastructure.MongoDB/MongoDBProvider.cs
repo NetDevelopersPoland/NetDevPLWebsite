e0aa02e1b8f69d1d5926a28090b92e80db7e6c8e
Pawel Klimczyk <pawel@klimczyk.pl>
2016-06-05 15:14:09 +0200

    Added NetDevPL.Infrastructure.MongoDB

---
+++ src/Infrastructure/NetDevPL.Infrastructure.MongoDB/MongoDBProvider.cs
@@ -0,0 +1,58 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace NetDevPL.Infrastructure.MongoDB
+{
+    public class MongoDBProvider<T>
+    {
+        private IMongoClient client;
+        private IMongoDatabase database;
+        private readonly string databaseName;
+
+        private readonly string collectionName;
+
+        public MongoDBProvider(string databaseName, string collectionName)
+        {
+            this.databaseName = databaseName;
+            this.collectionName = collectionName;
+
+            InitDB();
+        }
+
+        static MongoDBProvider()
+        {
+            BsonClassMap.RegisterClassMap<T>(cm => { cm.AutoMap(); });
+        }
+
+        public IMongoCollection<T> Collection
+        {
+            get
+            {
+                return database.GetCollection<T>(collectionName);
+            }
+        }
+
+        private void InitDB()
+        {
+
+            client = new MongoClient();
+
+            database = client.GetDatabase(databaseName);
+
+            if (!CollectionExists(database, collectionName))
+            {
+                CreateCollectionOptions options = new CreateCollectionOptions { AutoIndexId = true };
+                database.CreateCollection(collectionName, options);
+            }
+        }
+
+        private bool CollectionExists(IMongoDatabase db, string collName)
+        {
+            var filter = new BsonDocument("name", collName);
+            var collections = db.ListCollections(new ListCollectionsOptions { Filter = filter });
+
+            return collections.Any();
+        }
+    }
+}
