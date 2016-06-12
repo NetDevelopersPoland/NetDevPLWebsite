3493eef3d12216132fcdc84ba44ecc20b0323a35
Pawel Klimczyk <pawel@klimczyk.pl>
2016-06-12 10:01:28 +0200

    Added SharedKernel and NLog

---
+++ src/NetDevPLWeb.SharedKernel/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NetDevPLWeb.SharedKernel
+{
+    public static class Logger
+    {
+        public static void Fatal(string message)
+        {
+            NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+            logger.Fatal(message);
+        }
+
+        public static void Fatal(Exception excepion)
+        {
+            NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+            logger.Fatal(excepion);
+        }
+
+        public static void Info(string message)
+        {
+            NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+            logger.Info(message);
+        }
+
+        public static void Debug(string message)
+        {
+            NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+            logger.Debug(message);
+        }
+
+        public static void Warning(string message)
+        {
+            NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+            logger.Warn(message);
+        }
+    }
+}
