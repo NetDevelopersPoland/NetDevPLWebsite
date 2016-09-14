a8d51de1cd7e55e8261d3e987e9c076068fdf63f
Pawel Klimczyk <pawel@klimczyk.pl>
2016-09-14 10:19:12 +0200

    Added 'Resources' page

---
+++ src/NetDevPLWeb/Features/Resources/ResourcesModule.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Nancy;
+using Newtonsoft.Json;
+
+namespace NetDevPLWeb.Features.Resources
+{
+    public class ResourcesModule : NancyModule
+    {
+        ResourcesSource source = new ResourcesSource();
+
+        public ResourcesModule()
+        {
+            Get["/resources"] = parameters =>
+            {
+                var toolsMastering = source.GetMasteringTools();
+
+                return View["resourcesList", new ResourcesViewModel(toolsMastering)];
+            };
+        }
+    }
+
+    public class ResourcesSource
+    {
+        public List<Resource> GetMasteringTools()
+        {
+            string json = File.ReadAllText("Features/Resources/resources.json");
+            var resources = JsonConvert.DeserializeObject<List<Resource>>(json);
+            
+            return resources.ToList();
+        }
+    }
+
+    public class Resource
+    {
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class ResourcesViewModel
+    {
+        public ResourcesViewModel(List<Resource> resources)
+        {
+            ResourcesList = resources;
+        }
+
+        public List<Resource> ResourcesList { get; private set; }
+    }
+}
