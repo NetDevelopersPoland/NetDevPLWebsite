f14806a91982f12bc4d81c7b7c064db408374e49
jedmac <mac.jedrzejewski@gmail.com>
2016-10-01 15:15:04 +0200

    Complete refactoring on Blogs (#28)

---
+++ src/Apps/NetDevPL.Apps.WebApp/Features/Blogs/ViewModels/BlogPostViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NetDevPLWeb.Features.Blogs.ViewModels
+{
+    public class BlogPostViewModel
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+    }
+}
