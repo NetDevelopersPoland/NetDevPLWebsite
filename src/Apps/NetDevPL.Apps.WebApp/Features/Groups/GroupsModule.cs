0c31a4ff4d8e8e479902889782e43e159fb6eab7
Pawel Klimczyk <pawel@klimczyk.pl>
2016-10-06 17:16:50 +0200

    Fixed issue with time zone displayedon Groups page

--- src/Apps/NetDevPL.Apps.WebApp/Features/Groups/GroupsModule.cs
+++ src/Apps/NetDevPL.Apps.WebApp/Features/Groups/GroupsModule.cs
@@ -19,12 +19,22 @@ namespace NetDevPLWeb.Features.Groups
             };
         }
     }
-    
+
     public class GroupsViewModel
     {
         public GroupsViewModel(NetGroupDataSnapshot snapshot)
         {
-            GroupsList = snapshot.Groups?? new List<NetGroup>();
+            GroupsList = snapshot.Groups ?? new List<NetGroup>();
+
+            foreach (var netGroup in GroupsList)
+            {
+                foreach (var meeting in netGroup.UpcomingMeetings)
+                {
+                    //TimeZoneInfo.ConvertTimeFromUtc(meeting.Date, TimeZoneInfo.Local)
+                    meeting.Date = meeting.Date.ToLocalTime();
+                }
+            }
+
             LastUpdate = snapshot.SnapshotDate;
         }
 
