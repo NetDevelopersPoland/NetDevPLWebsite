c4e4a547d104749f5a505810c3e6aca01bdf7c95
Pawel Klimczyk <pawel@klimczyk.pl>
2016-10-13 13:07:09 +0200

    Testing openCover and codecov integration

--- scripts/runTests.ps1
+++ scripts/runTests.ps1
@@ -1,33 +1,33 @@
-param (
-    [Parameter(Mandatory=$true)][string]$putputFilename = ""
-)
+#$openCover = Get-ChildItem -Path "C:\Users\$([Environment]::UserName)\.nuget\packages\" -Filter "OpenCover.Console.exe" -Recurse | % { $_.FullName }
+#$xUnit = Get-ChildItem -Path "C:\Users\$([Environment]::UserName)\.nuget\packages\" -Filter "xunit.console.exe" -Recurse | % { $_.FullName }
 
-$openCover = Get-ChildItem -Path "C:\Users\$([Environment]::UserName)\.nuget\packages\" -Filter "OpenCover.Console.exe" -Recurse | % { $_.FullName }
-$xUnit = Get-ChildItem -Path "C:\Users\$([Environment]::UserName)\.nuget\packages\" -Filter "xunit.console.exe" -Recurse | % { $_.FullName }
-
-#$openCover = "..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe"
-#$xUnit = "..\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe"
+$openCover = "packages\OpenCover.4.6.519\tools\OpenCover.Console.exe"
+$openCoverProfiler = "packages\OpenCover.4.6.519\tools\x86\OpenCover.Profiler.dll"
+$xUnit = "packages\xunit.runner.console.2.1.0\tools\xunit.console.exe"
 
 # entry folder
-$root = "..\artifacts\"
+$root = "artifacts\"
+$coverageFile = $root + "NetDevPL_coverage.xml"
 
 # test projects to run with OpenCover
 $projects = @(
-    @{Path="NetDevPL.Features.NetGroups.IntegrationTests.dll"; Filter="+[NetDevPL]*"}
+    @{Path="NetDevPL.Features.NetGroups.IntegrationTests.dll"; Filter="+[NetDevPL*]*"}
 )
 
 function RunCodeCoverage($testProject, $filter) {
-    & $openCover "-target:$xUnit" "-targetargs:$testProject -noshadow -appveyor" "-output:$putputFilename" "-register:user" "-filter:$filter" -mergeoutput -oldStyle
+        & $openCover -register:Path32 "`"-target:$xUnit`"" "`"-targetargs:$testProject -noshadow -appveyor`""  "-filter:$filter" -output:"$coverageFile" -hideskipped:All -mergeoutput -oldStyle -returntargetcode
 }
 
 # run unit tests and calculate code coverage for each test project
 foreach ($project in $projects) {
-    RunCodeCoverage $($root + $project.Path) "+[NetDevPL]*"#$project.Filter
+    RunCodeCoverage $($root + $project.Path) "+[NetDevPL*]*" #$project.Filter
 }
 
 # try to find error
 if($LastExitCode -ne 0)
 {
    # mark build as failed
-   $host.SetShouldExit($LastExitCode)
+   #$host.SetShouldExit($LastExitCode)
+   
+   Write-Host $LastExitCode
 }
