4508e8175680a678b42b9f7f0da763b9f12b2c4f
Pawel Klimczyk <pawel@klimczyk.pl>
2016-10-13 11:37:54 +0200

    Added OpenCover + script

---
+++ tests/NetDevPL.TestHelpers/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NetDevPL.TestHelpers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetDevPL.TestHelpers")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5aba53d4-ef59-4d96-9535-7e9fcedb245c")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
