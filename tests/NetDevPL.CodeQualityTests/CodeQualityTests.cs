f93b9e73ca70f783af86d5907ca40aa5f0cc40ef
Pawel Klimczyk <pawel@klimczyk.pl>
2016-10-30 07:06:23 +0100

    Updated CodeWatch and added more code quality checks

--- tests/NetDevPL.CodeQualityTests/CodeQualityTests.cs
+++ tests/NetDevPL.CodeQualityTests/CodeQualityTests.cs
@@ -1,4 +1,6 @@
-﻿using Gmtl.CodeWatch;
+﻿using System;
+using Gmtl.CodeWatch;
+using Gmtl.CodeWatch.Watchers;
 using Xunit;
 
 namespace NetDevPL.CodeQualityTests
@@ -8,12 +10,28 @@ namespace NetDevPL.CodeQualityTests
         [Fact]
         public void PropertyNamesShouldStartWithUppercase()
         {
-            PropertyNamingWatcher watcher = new PropertyNamingWatcher();
-            watcher.Configure(Naming.UpperCase);
-            watcher.WatchAssembly(typeof(NetDevPLWeb.Program).Assembly);
-            watcher.WatchAssembly(typeof(NetDevPLBackgoundJobs.Program).Assembly);
+            CodeWatcherConfig config = CodeWatcherConfig.Create()
+                .WithWatcher(ctx => new PropertyNamingWatcher().Configure(Naming.UpperCase))
+                .WatchAssembly(typeof(NetDevPLWeb.Program).Assembly)
+                .WatchAssembly(typeof(NetDevPLBackgoundJobs.Program).Assembly)
+                .WatchAssembly(typeof(NetDevPL.Features.Blogs.Blog).Assembly)
+                .WatchAssembly(typeof(NetDevPL.Features.Facebook.FacebookPost).Assembly)
+                .WatchAssembly(typeof(NetDevPL.Features.NetGroups.Group).Assembly)
+                .WatchAssembly(typeof(NetDevPL.Infrastructure.Helpers.RssProvider).Assembly)
+                .WatchAssembly(typeof(NetDevPL.Infrastructure.MongoDB.MongoDBProvider<object>).Assembly)
+                .WatchAssembly(typeof(NetDevPL.Infrastructure.SharedKernel.Logger).Assembly)
+                .WatchAssembly(typeof(NetDevPL.Features.NetGroups.IntegrationTests.MeetupDataProviderTests).Assembly)
 
-            watcher.Execute();
+                //Types below are skipped till json mapping/attributes are added there
+                .SkipType(typeof(NetDevPL.Features.NetGroups.Group))
+                .SkipType(typeof(NetDevPL.Features.NetGroups.Meta))
+                .SkipType(typeof(NetDevPL.Features.NetGroups.RootObject))
+                .SkipType(typeof(NetDevPL.Features.NetGroups.Venue))
+                .SkipType(typeof(NetDevPL.Features.NetGroups.Result))
+                .Build();
+
+            config.Execute();
+
         }
     }
 }
