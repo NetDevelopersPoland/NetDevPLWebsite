3b51547c830fa709ebeea58a09b2d340df5fb431
Pawel Klimczyk <pawel@klimczyk.pl>
2016-11-15 16:43:12 +0100

    Added Reporting Feature dll

---
+++ src/Features/NetDevPL.Features.Reporting/DomainModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetDevPL.Features.Reporting
+{
+    public class KarmaReport
+    {
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public IList<UserKarma> TopUsers { get; set; }
+    }
+    
+    public class UserKarma
+    {
+        public string Name { get; set; }
+        public int KarmaPoints { get; set; }
+    }
+}
