8166347840788ea9d8956d5df987b212ead4d87e
Arkadiusz Benedykt <abenedykt@rekord.com.pl>
2016-12-06 12:14:54 +0100

    removing redundant qualifiers

--- tests/NetDevPL.CodeQualityTests/CodeQualityTests.cs
+++ tests/NetDevPL.CodeQualityTests/CodeQualityTests.cs
@@ -13,20 +13,20 @@ namespace NetDevPL.CodeQualityTests
                 .WithWatcher(ctx => new PropertyNamingWatcher().Configure(Naming.UpperCase))
                 .WatchAssembly(typeof(NetDevPLWeb.Program).Assembly)
                 .WatchAssembly(typeof(NetDevPLBackgoundJobs.Program).Assembly)
-                .WatchAssembly(typeof(NetDevPL.Features.Blogs.Blog).Assembly)
-                .WatchAssembly(typeof(NetDevPL.Features.Facebook.FacebookPost).Assembly)
-                .WatchAssembly(typeof(NetDevPL.Features.NetGroups.Group).Assembly)
-                .WatchAssembly(typeof(NetDevPL.Infrastructure.Helpers.RssProvider).Assembly)
-                .WatchAssembly(typeof(NetDevPL.Infrastructure.MongoDB.MongoDBProvider<object>).Assembly)
-                .WatchAssembly(typeof(NetDevPL.Infrastructure.SharedKernel.Logger).Assembly)
-                .WatchAssembly(typeof(NetDevPL.Features.NetGroups.IntegrationTests.MeetupDataProviderTests).Assembly)
+                .WatchAssembly(typeof(Features.Blogs.Blog).Assembly)
+                .WatchAssembly(typeof(Features.Facebook.FacebookPost).Assembly)
+                .WatchAssembly(typeof(Features.NetGroups.Group).Assembly)
+                .WatchAssembly(typeof(Infrastructure.Helpers.RssProvider).Assembly)
+                .WatchAssembly(typeof(Infrastructure.MongoDB.MongoDBProvider<object>).Assembly)
+                .WatchAssembly(typeof(Infrastructure.SharedKernel.Logger).Assembly)
+                .WatchAssembly(typeof(Features.NetGroups.IntegrationTests.MeetupDataProviderTests).Assembly)
 
                 //Types below are skipped till json mapping/attributes are added there
-                .SkipType(typeof(NetDevPL.Features.NetGroups.Group))
-                .SkipType(typeof(NetDevPL.Features.NetGroups.Meta))
-                .SkipType(typeof(NetDevPL.Features.NetGroups.RootObject))
-                .SkipType(typeof(NetDevPL.Features.NetGroups.Venue))
-                .SkipType(typeof(NetDevPL.Features.NetGroups.Result))
+                .SkipType(typeof(Features.NetGroups.Group))
+                .SkipType(typeof(Features.NetGroups.Meta))
+                .SkipType(typeof(Features.NetGroups.RootObject))
+                .SkipType(typeof(Features.NetGroups.Venue))
+                .SkipType(typeof(Features.NetGroups.Result))
                 .Build();
 
             config.Execute();
