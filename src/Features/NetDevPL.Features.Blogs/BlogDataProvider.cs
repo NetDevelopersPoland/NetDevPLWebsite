c21d8c9e2c5c963c497f71a178dae139dbb4eee2
Michał Komorowski <michaliga@o2.pl>
2016-12-18 21:28:34 +0100

    Before my changes BlogDataProvider assumed that SyndicationItem.Id contains an url of a post. However it is not always true. I changed the implementation so that an url can be retrieved from SyndicationItem.Links collection. BlogDataProvider  will do so if Blog.IndexOfLinkToPost is set to a correct value.

--- src/Features/NetDevPL.Features.Blogs/BlogDataProvider.cs
+++ src/Features/NetDevPL.Features.Blogs/BlogDataProvider.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Syndication;
+
 using NetDevPL.Infrastructure.Helpers;
+
 
 namespace NetDevPL.Features.Blogs
 {
@@ -11,12 +15,26 @@ namespace NetDevPL.Features.Blogs
             foreach (var blog in blogs)
             {
                 var rssData = RssProvider.GetItemsFromRss(blog.Rss);
-                var blogPosts = rssData.Select(item => new BlogPost {Title = item.Title.Text, Url = item.Id, PublishDate = item.PublishDate.UtcDateTime});
+                var blogPosts = rssData.Select(item => new BlogPost {Title = item.Title.Text, Url = GetUrl(blog, item), PublishDate = item.PublishDate.UtcDateTime});
 
                 blog.BlogPosts = blogPosts;
             }
 
             return blogs;
+        }
+
+        private string GetUrl(Blog blog, SyndicationItem item)
+        {
+            var url = item.Id;
+
+            if (blog.IndexOfLinkToPost.HasValue)
+            {
+                var index = blog.IndexOfLinkToPost.Value;
+                if (item.Links != null && item.Links.Any() && index < item.Links.Count)
+                    url = item.Links[index].Uri.AbsoluteUri;
+            }
+
+            return url;
         }
     }
 }
