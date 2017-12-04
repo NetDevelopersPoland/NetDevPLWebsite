c4b254e3ea9f6e81ffb6a04cf220f7b98c4d130a
Pawel Klimczyk <pawel@klimczyk.pl>
2017-12-04 17:44:01 +0100

    Added exception handling and logging in SampleAuthenticationCallbackProvider

--- src/Apps/NetDevPL.Apps.WebApp/SampleAuthenticationCallbackProvider.cs
+++ src/Apps/NetDevPL.Apps.WebApp/SampleAuthenticationCallbackProvider.cs
@@ -3,6 +3,7 @@ using Nancy;
 using Nancy.Authentication.Forms;
 using Nancy.SimpleAuthentication;
 using NetDevPL.Features.UserManagement;
+using NetDevPL.Infrastructure.SharedKernel;
 
 namespace NetDevPLWeb
 {
@@ -10,34 +11,57 @@ namespace NetDevPLWeb
     {
         public dynamic Process(NancyModule nancyModule, AuthenticateCallbackData model)
         {
-            //TODO handle exception from model.Exception
-            Repository repo = new Repository();
-
-            User user = repo.GetByMail(model.AuthenticatedClient.UserInformation.Email);
-
-            if (user == null)
+            try
             {
-                user = new User
+                if (model.Exception != null)
                 {
-                    Id = Guid.NewGuid(),
-                    CreateDate = DateTime.UtcNow,
-                    Email = model.AuthenticatedClient.UserInformation.Email,
-                    Name = model.AuthenticatedClient.UserInformation.Name,
-                    Picture = model.AuthenticatedClient.UserInformation.Picture,
-                    Provider = model.AuthenticatedClient.ProviderName,
-                    ProviderExternalId = model.AuthenticatedClient.UserInformation.Id
-                };
+                    Logger.Fatal(model.Exception);
+                    return ReturnLoginErrorResponse();
+                }
+                else
+                {
+                    //TODO handle exception from model.Exception
+                    Repository repo = new Repository();
 
-                repo.Add(user);
+                    User user = repo.GetByMail(model.AuthenticatedClient.UserInformation.Email);
+
+                    if (user == null)
+                    {
+                        user = new User
+                        {
+                            Id = Guid.NewGuid(),
+                            CreateDate = DateTime.UtcNow,
+                            Email = model.AuthenticatedClient.UserInformation.Email,
+                            Name = model.AuthenticatedClient.UserInformation.Name,
+                            Picture = model.AuthenticatedClient.UserInformation.Picture,
+                            Provider = model.AuthenticatedClient.ProviderName,
+                            ProviderExternalId = model.AuthenticatedClient.UserInformation.Id
+                        };
+
+                        repo.Add(user);
+                    }
+
+                    return nancyModule.LoginAndRedirect(user.Id);
+                }
             }
-            
-
-            return nancyModule.LoginAndRedirect(user.Id);
+            catch (Exception exc)
+            {
+                Logger.Fatal(exc);
+                return ReturnLoginErrorResponse();
+            }
         }
 
         public dynamic OnRedirectToAuthenticationProviderError(NancyModule nancyModule, string errorMessage)
         {
             return null;
+        }
+
+        private dynamic ReturnLoginErrorResponse()
+        {
+            var r = (Response)"Could not authenticate. Notify administrator, please :)";
+            r.StatusCode = HttpStatusCode.InternalServerError;
+
+            return r;
         }
     }
 }
